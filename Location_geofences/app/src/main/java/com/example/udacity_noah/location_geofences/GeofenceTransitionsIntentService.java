09ebcf55a500716b29ce40c678eaf39b7363d509
Noah Patterson <n@noahpatterson.me>
2015-10-30 16:13:34 -0400

    init

---
+++ Location_geofences/app/src/main/java/com/example/udacity_noah/location_geofences/GeofenceTransitionsIntentService.java
@@ -0,0 +1,44 @@
+package com.example.udacity_noah.location_geofences;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p/>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class GeofenceTransitionsIntentService extends IntentService {
+
+    public GeofenceTransitionsIntentService() {
+        super("GeofenceTransitionsIntentService");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+        if (geofencingEvent.hasError()) {
+            return;
+        }
+
+        // get the transition type
+        int geofenceTransition = geofencingEvent.getGeofenceTransition();
+
+        //check if transition is enter or exit
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER || geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+            
+        }
+    }
+}
